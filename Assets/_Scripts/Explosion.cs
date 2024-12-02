01b32b82f6b773b6a6eea2e583eb37d24e3b4b19
Evan Wheeler <ewheeler121@outlook.com>
2024-12-02 13:56:08 -0500

    More tuning

--- Assets/_Scripts/Explosion.cs
+++ Assets/_Scripts/Explosion.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
 	public float maxForce = 750f;
-    public float maxMagnitude = 3f;
+    public float minMagnitude = 15f;
 	public float explosionRadius = 3f;
 	void Start()
 	{
 //		Debug.Log(Camera.main);
 		if (!Camera.main)
 			return;
-			
 		Vector3 direction = (Camera.main.transform.parent.position + Vector3.up) - transform.position;
-        direction.Normalize();
-        direction *= maxMagnitude;
-	//	Debug.Log(direction + " " + direction.magnitude);
+        //	Debug.Log(direction + " " + direction.magnitude);
 		if (direction.magnitude <= explosionRadius)
 		{
+            direction.Normalize();
+            direction *= minMagnitude;
 			FirstPersonController firstPersonController = Camera.main.transform.parent.GetComponent<FirstPersonController>();
 			firstPersonController.AddExternalForce(direction * maxForce);
 		}
